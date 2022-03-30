3ca1cc19c0db580382235a61b609a8378a120fe3
Igor Silva <danieligord7@hotmail.com>
2022-03-30 06:42:38 -0300

    Transform Commit

---
+++ 14-transform/transform.js
@@ -0,0 +1,20 @@
+function moveUP(){
+    let DivItem = document.getElementById('item')
+    DivItem.style.transform = "translateY(-100px)"
+}
+function moveRIGHT(){
+    let DivItem = document.getElementById('item')
+    DivItem.style.transform = "translateX(100px)"
+}
+function moveDOWN(){
+    let DivItem = document.getElementById('item')
+    DivItem.style.transform = "translateY(100px)"
+}
+function moveLEFT(){
+    let DivItem = document.getElementById('item')
+    DivItem.style.transform = "translateX(-100px)"
+}
+function moveCENTER(){
+    let DivItem = document.getElementById('item')
+    DivItem.style.transform = "translateX(0px)"
+}
---
+++ 14-transform/transform.md
@@ -0,0 +1,7 @@
+# Transform Propertie Notes
+
+## ALLOWS YOU WHAT?
+
+- The transform property lets you rotate, scale, skew or translate an element. It modifies the coordinate space of the CSS <em>visual formattting model</em>.
+- <strong>transform: </strong> 
+- Into the transform you can use <strong>rotate</strong>, <strong>translate</strong>, etc.
